85c75acf5628c4a77dc04a2157c7f7f2fe74e230
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-12-01 18:23:18 -0500

    test: updated  test for integrating a linear function

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 def test_integrate_linear_():
-    result = integrate_linear_(4, 2, 1, 5)
-    assert abs(result - 72.0) < 1e-5 
+    result = integrate_linear(4, 2, 1, 5)
+    assert abs(result - 56.0) < 1e-5 
